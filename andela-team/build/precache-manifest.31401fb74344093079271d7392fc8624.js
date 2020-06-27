d973de8f52c0144ab7a019d5a23b4e3f6d526b2e
NATFINEST <ese.nat4@gmail.com>
2020-06-27 17:39:43 +0100

    up

---
+++ andela-team/build/precache-manifest.31401fb74344093079271d7392fc8624.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96e379292e755c3fc935d6a26346906",
+    "url": "/andela-team/index.html"
+  },
+  {
+    "revision": "c1d09cc43f6b2337728e",
+    "url": "/andela-team/static/css/main.2c381b1b.chunk.css"
+  },
+  {
+    "revision": "a4ea77a017ca48aa6c48",
+    "url": "/andela-team/static/js/2.622bd2fb.chunk.js"
+  },
+  {
+    "revision": "e4a9bfd8592cbc6d958937c4455d5ca8",
+    "url": "/andela-team/static/js/2.622bd2fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d09cc43f6b2337728e",
+    "url": "/andela-team/static/js/main.9ea31e90.chunk.js"
+  },
+  {
+    "revision": "9509f45b380f5c7fe71c",
+    "url": "/andela-team/static/js/runtime-main.dea51d2b.js"
+  },
+  {
+    "revision": "ce9054d4ef869a46f574d5b4528c0fe4",
+    "url": "/andela-team/static/media/article.ce9054d4.jpg"
+  },
+  {
+    "revision": "62c60e3fde1682dc5abad9dde9ba02fa",
+    "url": "/andela-team/static/media/avatar.62c60e3f.jpg"
+  },
+  {
+    "revision": "c1a38c174b0fcf25063751460728aaf3",
+    "url": "/andela-team/static/media/code.c1a38c17.jpg"
+  },
+  {
+    "revision": "70c587da55fea3f8b2105c9b77703ac5",
+    "url": "/andela-team/static/media/home-banner.70c587da.jpg"
+  }
+]);
--- andela-team/build/service-worker.js
+++ andela-team/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.009cc02c9f59d8b2deaba58b24cbb884.js"
+  "/andela-team/precache-manifest.31401fb74344093079271d7392fc8624.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/andela-team/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
